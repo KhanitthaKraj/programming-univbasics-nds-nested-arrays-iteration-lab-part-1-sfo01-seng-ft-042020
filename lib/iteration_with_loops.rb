aca83133a275b65de289d26e7dd8224163e2b0c8
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-14 04:53:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,4 +7,13 @@ def find_even_values(src)
     [99, 50, 3, 4],
     [23, 41]
   ]
+  count = 0
+  
+  while count < src.length do
+    inner_count = 0 
+    while inner_count < src[count].length do
+      p 
+    end
+  end
+  count += 1
 end
